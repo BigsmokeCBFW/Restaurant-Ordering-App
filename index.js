804a7a9d90bd49421654eff9ede7aa907e7bb4eb
Ahmed Omeiza <omeizaahmed81@gmail.com>
2025-08-06 19:03:24 +0100

    default state

---
+++ index.js
@@ -0,0 +1,22 @@
+import { menuArray } from "./data.js";
+
+const menuEl = document.getElementById('menu-container')
+
+function render() {
+  menuEl.innerHTML = menuArray.map((menu) =>{
+    const {name, ingredients, price, emoji } = menu
+    return `
+      <div class="menu">
+                    <h3 class="emoji">${emoji}</h3>
+                    <span class="menu-detail">
+                        <h4 class="menu-title">${name}</h4>
+                        <h4 class="menu-ingredient">${ingredients}</h4>
+                        <h4 class="menu-price">$${price}</h4>
+                    </span>
+                    <button class="add-menu">+</button>
+                </div>
+    `
+  } ).join("")
+}
+
+render()
